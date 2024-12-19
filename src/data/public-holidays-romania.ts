18dc9d855d085ad454c8a0e08ff1828b935942e3
kulcsarrudolf <kulcsarrudolf@gmail.com>
2024-12-19 17:34:18 +0200

    Added public holidays

---
+++ src/data/public-holidays-romania.ts
@@ -0,0 +1,147 @@
+export const publicHolidaysRomania = [
+  {
+    name: {
+      ro: 'Anul Nou',
+    },
+    dates: ['2025-01-01', '2025-01-02'],
+  },
+  {
+    name: {
+      ro: 'Boboteaza',
+    },
+    dates: ['2025-01-06'],
+  },
+  {
+    name: {
+      ro: 'Sfantul Ioan Botezatorul',
+    },
+    dates: ['2025-01-07'],
+  },
+  {
+    name: {
+      ro: 'Ziua Unirii Principatelor Române',
+    },
+    dates: ['2025-01-24'],
+  },
+  {
+    name: {
+      ro: 'Paște ortodox',
+    },
+    dates: ['2025-04-18', '2025-04-19', '2025-04-20', '2025-04-21'],
+  },
+  {
+    name: {
+      ro: 'Ziua Muncii',
+    },
+    dates: ['2025-05-01'],
+  },
+  {
+    name: {
+      ro: 'Ziua Copilului',
+    },
+    dates: ['2025-06-01'],
+  },
+  {
+    name: {
+      ro: 'Rusalii',
+    },
+    dates: ['2025-06-08', '2025-06-09'],
+  },
+  {
+    name: {
+      ro: 'Adormirea Maicii Domnului',
+    },
+    dates: ['2025-08-15'],
+  },
+  {
+    name: {
+      ro: 'Sfântul Andrei',
+    },
+    dates: ['2025-11-30'],
+  },
+  {
+    name: {
+      ro: 'Ziua Națională a României',
+    },
+    dates: ['2025-12-01'],
+  },
+  {
+    name: {
+      ro: 'Crăciunul',
+    },
+    dates: ['2025-12-25', '2025-12-26'],
+  },
+
+  {
+    name: {
+      ro: 'Anul Nou',
+    },
+    dates: ['2026-01-01', '2026-01-02'],
+  },
+  {
+    name: {
+      ro: 'Boboteaza',
+    },
+    dates: ['2026-01-06'],
+  },
+  {
+    name: {
+      ro: 'Sfantul Ioan Botezatorul',
+    },
+    dates: ['2026-01-07'],
+  },
+  {
+    name: {
+      ro: 'Ziua Unirii Principatelor Române',
+    },
+    dates: ['2026-01-24'],
+  },
+  {
+    name: {
+      ro: 'Paște ortodox',
+    },
+    dates: ['2026-04-10', '2026-04-11', '2026-04-12', '2026-04-13'],
+  },
+  {
+    name: {
+      ro: 'Ziua Muncii',
+    },
+    dates: ['2026-05-01'],
+  },
+  {
+    name: {
+      ro: 'Ziua Copilului',
+    },
+    dates: ['2026-06-01'],
+  },
+  {
+    name: {
+      ro: 'Rusalii',
+    },
+    dates: ['2026-05-31', '2026-06-01'],
+  },
+  {
+    name: {
+      ro: 'Adormirea Maicii Domnului',
+    },
+    dates: ['2026-08-15'],
+  },
+  {
+    name: {
+      ro: 'Sfântul Andrei',
+    },
+    dates: ['2026-11-30'],
+  },
+  {
+    name: {
+      ro: 'Ziua Națională a României',
+    },
+    dates: ['2026-12-01'],
+  },
+  {
+    name: {
+      ro: 'Crăciunul',
+    },
+    dates: ['2026-12-25', '2026-12-26'],
+  },
+];
